71251613487964b43c0f747bce4848c18022fa19
Akihiko Nakamura <akihikonakamura@akihikoomacbook.home>
2019-12-16 00:21:27 -0500

    Q6 to Q10

--- kadai.rb
+++ kadai.rb
@@ -53,16 +53,68 @@ def q5
     fin
 end
 
+#Q6. 配列が空であればtrue、1つ以上の要素があればfalseを返すコードを書いてください
+def q6(arr)
+    puts __method__
+    #検証用
+    # puts "#{arr}is empty?:#{arr.empty?}"
+    puts arr.empty?
+    fin
+end
 
+q6([])
+q6([1, 5, 8, 10])
 
+#Q7. 配列であればtrue、配列でなければfalseを返すコードを書いてください
+def q7(input)
+    puts __method__
+    #検証用
+    # puts "#{input}is Array?:#{input.kind_of?(Array)}"
+    puts input.kind_of?(Array)
+    fin
+end
 
+q7([])
+q7({})
 
+#Q8. mapとは異なるメソッドを使って以下と全く同じ処理を実現させてください
+    # numbers = ["6", "5", "3", "7", "1"]
+    # p numbers.map {|item| item.to_i }
+    # -> [6, 5, 3, 7, 1]
 
+def q8
+    puts __method__
+    numbers = ["6", "5", "3", "7", "1"]
+    arr = []
+    numbers.each {|item| arr << item.to_i}
+    p arr
+    fin
+end
 
-q1
-q2
-q3
-q4
-q5
+q8
 
+#Q9. 以下の配列を用いて、期待通りの出力結果になるようにコードを書いてください
+# ["田中", "佐藤", "佐々木", "高橋"]
+# 会員No.1 田中さん
+# 会員No.2 佐藤さん
+# 会員No.3 佐々木さん
+# 会員No.4 高橋さん
+def q9
+    puts __method__
+    members = ["田中", "佐藤", "佐々木", "高橋"]
+    members.each.with_index(1) do |member, i|
+        puts "会員No.#{i} #{member}さん"
+    end
+    fin
+end
 
+q9
+
+#Q10. 以下の配列の最後に山下を追加してください
+def q10
+    puts __method__
+    members = ["田中", "佐藤", "佐々木", "高橋"]
+    p members << "山下"
+end
+
+q10
